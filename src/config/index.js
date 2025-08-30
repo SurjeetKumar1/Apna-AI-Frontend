4d5d86b1ae1a74c8c8175482c8226b11b33274e8
Surjeet <surjeet@Surjeets-MacBook-Air.local>
2025-08-30 16:59:38 +0530

    add EC2 DNS

--- src/config/index.js
+++ src/config/index.js
@@ -1,1 +1,2 @@
-export const BACKEND_URL="http://localhost:8080"
+// export const BACKEND_URL="http://localhost:8080"
+export const BACKEND_URL="https://ec2-54-210-6-159.compute-1.amazonaws.com:8080"
