2e1d47aff67fb8c35696a732b7e2f864522203a3
Surjeet <surjeet@Surjeets-MacBook-Air.local>
2025-09-02 14:30:42 +0530

    add render url

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,3 @@
 // export const BACKEND_URL="http://localhost:8080"
-export const BACKEND_URL="http://ec2-54-210-6-159.compute-1.amazonaws.com:8080"
+// export const BACKEND_URL="http://ec2-54-210-6-159.compute-1.amazonaws.com:8080"
+export const BACKEND_URL="https://apna-ai-backend.onrender.com"
